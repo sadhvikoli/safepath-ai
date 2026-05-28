45f10ca7adb707ac88c6ea097946737427258670
sadhvi koli <sadhvikoli@sadhvis-MacBook-Air.local>
2026-05-27 21:35:05 -0400

    Fix CORS for deployed frontend

--- backend/main.py
+++ backend/main.py
@@ -23,7 +23,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://YOUR-VERCEL-URL.vercel.app"
+        "https://safepath-ndm95jjuv-sadhvikolis-projects.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
